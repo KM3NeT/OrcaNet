59d6744ef766684cd7f86bb2064fb19052da099d
ViaFerrata <michimoser@onlinehome.de>
2018-06-19 10:07:05 +0200

    - Adds support for multiple input files WITH the data split to multiple files, such as file_0_dset_0 file_0_dset_1 ... file_1_dset_0 file_1_dset_1 ... - zero_mean array is not copied to the ssd anymore, since it is held in memory anyways

--- HPC/cnns/utilities/input_utilities.py
+++ HPC/cnns/utilities/input_utilities.py
@@ -27,8 +27,18 @@ def parse_input():
     The output (train_files, test_files) is structured as follows:
     1) train/test: [ ( [train/test_filepath]  , n_rows) ]. The outmost list has len 1 as well as the list in the tuple.
     2) train/test: [ ( [train/test_filepath]  , n_rows), ... ]. The outmost list has arbitrary length (depends on number of files), but len 1 for the list in the tuple.
-    3) train/test: [ ( [train/test_filepath]  , n_rows), ... ]. The outmost list has len 1, but the list inside the tuple has arbitrary length.
-       A combination of 2) + 3) (multiple input files for each batch from 3) AND all events split over multiple files) is not yet supported.
+       The two train and test input .list files should be structured as follows:
+            file_0 \n
+            file_1 \n
+            file_2 # finish, no \n at last line!
+
+    3) train/test: [ ( [train/test_filepath]  , n_rows), ... ]. The outmost list has arbitrary length, as well as the list inside the tuple.
+       The two train and test input .list files should be structured as follows:
+            file_0_dset_0 \n
+            file_0_dset_1 \n
+            \n
+            file_1_dset_0 \n
+            file_1_dset_1 # finish, no \n at last line!
 
     :return: list(([train_filepaths], train_filesize)) train_files: list of tuples that contains the list(trainfiles) and their number of rows.
     :return: list(([test_filepaths], test_filesize)) test_files: list of tuples that contains the list(testfiles) and their number of rows.
@@ -43,12 +53,11 @@ def parse_input():
     parser.add_argument('train_file', metavar='train_file', type=str, nargs='?', help='the filepath of the traindata file.')
     parser.add_argument('test_file', metavar='test_file', type=str, nargs='?', help='the filepath of the testdata file.')
     parser.add_argument('-l', '--list', dest='listfile_train_and_test', type=str, nargs=2,
-                        help='filepath of a .list file that contains all .h5 files that should be used for training/testing.')
-    parser.add_argument('-m', '--multiple_files', dest='listfile_multiple', type=str, nargs=1,
-                        help='filepath of a .list file that which contains multiple input files '
+                        help='filepaths of two .list files (train / test) that contains all .h5 files that should be used for training/testing.')
+    parser.add_argument('-m', '--multiple_files', dest='listfile_multiple', type=str, nargs=2,
+                        help='filepaths of two .list files where each contains multiple input files '
                              ' that should be used for training/testing in double/triple/... input models that need multiple input files per batch. \n'
-                             'The structure of the .list file should be as follows: \n '
-                             'f_1_train \n, f_1_test \n, f_2_train \n, f_2_test \n, ..')
+                             'The required structure of the .list files can be found in /utilities/input_utilities parse_input()')
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -74,16 +83,27 @@ def parse_input():
         train_files, test_files = [], []
         train_files_temp, test_files_temp = [], []
 
-        for i, line in enumerate(open(args.listfile_multiple[0])):
+        for line in open(args.listfile_multiple[0]):
             line = line.rstrip('\n')
-            if i % 2 == 0: # even, train_file
-                train_files_temp.append(line)
-            else: # odd, test_file
-                test_files_temp.append(line)
 
-        n_rows_train, n_rows_test = h5_get_number_of_rows(train_files_temp[0]), h5_get_number_of_rows(test_files_temp[0])
-        train_files.append((train_files_temp, n_rows_train))
-        test_files.append((test_files_temp, n_rows_test))
+            if line == '': # newline separator which specifies that one set of different cnn input files has been finished
+                # append one tuple (len > 1) to the train_files list for a certain datasplit number (e.g. file_0)
+                train_files.append((train_files_temp, h5_get_number_of_rows(train_files_temp[0])))
+                train_files_temp = []
+                continue
+
+            train_files_temp.append(line)
+
+        for line in open(args.listfile_multiple[1]):
+            line = line.rstrip('\n')
+
+            if line == '':  # newline separator which specifies that one set of different cnn input files has been finished
+                # append one tuple (len > 1) to the train_files list for a certain datasplit number (e.g. file_0)
+                test_files.append((test_files_temp, h5_get_number_of_rows(test_files_temp[0])))
+                test_files_temp = []
+                continue
+
+            test_files_temp.append(line)
 
     else:
         train_files = [([args.train_file], h5_get_number_of_rows(args.train_file))]
@@ -131,6 +151,7 @@ def use_node_local_ssd_for_input(train_files, test_files, multiple_inputs=False)
                 f_paths_train_ssd_temp.append(f_path_ssd)
 
             train_files_ssd.append((f_paths_train_ssd_temp, f_size)) # f_size of all f_paths should be the same
+            f_paths_train_ssd_temp = []
 
         for file_tuple in test_files:
             input_filepaths, f_size = file_tuple[0], file_tuple[1]
@@ -141,13 +162,7 @@ def use_node_local_ssd_for_input(train_files, test_files, multiple_inputs=False)
                 f_paths_test_ssd_temp.append(f_path_ssd)
 
             test_files_ssd.append((f_paths_test_ssd_temp, f_size)) # f_size of all f_paths should be the same
-
-        # copy xs_mean arrays to local ssd
-        for i, file_input_train in enumerate(train_files[0][0]):
-            files_npy = glob.iglob(os.path.join(os.path.dirname(file_input_train), '*.npy'))
-            for f in files_npy:
-                if os.path.isfile(f):
-                    shutil.copy2(f, local_scratch_path)
+            f_paths_test_ssd_temp = []
 
     else:
         for file_tuple in train_files:
@@ -163,12 +178,6 @@ def use_node_local_ssd_for_input(train_files, test_files, multiple_inputs=False)
             shutil.copy2(input_filepath, local_scratch_path) # copy to /scratch node-local SSD
             input_filepath_ssd = local_scratch_path + '/' + os.path.basename(input_filepath)
             test_files_ssd.append(([input_filepath_ssd], f_size))
-
-        # copy xs_mean array to local ssd
-        files_npy = glob.iglob(os.path.join(os.path.dirname(train_files[0][0][0]), '*.npy'))
-        for f in files_npy:
-            if os.path.isfile(f):
-                shutil.copy2(f, local_scratch_path)
 
     print 'Finished copying the input train/test data to the node-local SSD scratch folder'
     return train_files_ssd, test_files_ssd
