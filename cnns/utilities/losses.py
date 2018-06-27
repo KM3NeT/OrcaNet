f82992d517aa6181a7b1562d6d59155f7ab20914
ViaFerrata <michimoser@onlinehome.de>
2018-06-27 14:26:07 +0200

    Added preliminary loss function for uncertainty estimation, not finished yet!

--- cnns/utilities/losses.py
+++ cnns/utilities/losses.py
@@ -4,5 +4,11 @@ import keras.backend as K
 def custom_metric_mean_relative_error_5_labels(y_true, y_pred):
     return K.abs(y_pred - y_true)/(y_true[0])
 
+def loss_uncertainty_gaussian_likelihood(y_true, y_pred):
+    y_pred_label = y_pred[0] # TODO PLACEHOLDER!!
+    y_pred_label_std = y_pred[1] # TODO PLACEHOLDER!!
+    y_true_label = y_true[0] # TODO PLACEHOLDER!!
+    return K.log(y_pred_label_std ** 2) + (y_pred_label - y_true_label) ** 2 / (y_pred_label_std ** 2)
+
 
 
