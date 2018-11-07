222dc5366c34c0d7234a5df861fe279fa5da7eea
Tamas Gal <tgal@km3net.de>
2018-11-07 13:31:38 +0100

    Resolve "Add Sphinx documentation"

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as fobj:
+    requirements = [l.strip() for l in fobj.readlines()]
+
 setup(
     name='cnns',
     version='1.0',
     description='Runs CNNs',
-    url='https://github.com/ViaFerrata/OrcaNet/tree/master/cnns',
+    url='https://git.km3net.de/mmoser/OrcaNet',
     author='Michael Moser',
     author_email='mmoser@km3net.de',
     license='AGPL',
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
 )
-
