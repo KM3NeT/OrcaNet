152cb4c82094855a4a489f9d2dce10a7e93ada4a
ViaFerrata <michimoser@onlinehome.de>
2019-01-29 11:19:19 +0100

    Fix pypi versioning.

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     name='orcanet',
     description='Runs Neural Networks for usage in the KM3NeT project',
     url='https://git.km3net.de/ml/OrcaNet',
-    version='1.0',
     author='Michael Moser, Stefan Reck',
     author_email='mmoser@km3net.de, michael.m.moser@fau.de, stefan.reck@fau.de',
     license='AGPL',
@@ -17,17 +16,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
 
-    # setup_requires=['setuptools_scm'],
-    # use_scm_version={
-    #     'write_to': 'orcanet/version.txt',
-    #     'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
-    # },
-    # classifiers=[
-    #     'Development Status :: 3 - Alpha',
-    #     'Intended Audience :: Developers',
-    #     'Intended Audience :: Science/Research',
-    #     'Programming Language :: Python',
-    #]
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+        'write_to': 'orcanet/version.txt',
+        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+    ]
 )
 
 __author__ = 'Michael Moser and Stefan Reck'
