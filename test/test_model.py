45f096e2d7165565940e8ce8a20abb2d968d6c0d
Stefan Reck <stefan.reck@fau.de>
2019-01-31 12:42:08 +0100

    Started to change eval functions.

--- test/test_model.py
+++ test/test_model.py
@@ -18,7 +18,7 @@ def build_test_model():
 
 
 def get_structured_array():
-    x = np.array([(9, 81.0), (3, 18)],
+    x = np.array([(9, 81.0), (3, 18),  (3, 18),  (3, 18)],
                  dtype=[('inp_0', 'f4'), ('inp_1', 'f4')])
     return x
 
@@ -37,3 +37,13 @@ x = get_structured_array()
 xd = get_dict()
 model = build_test_model()
 
+y_pred = model.predict_on_batch(x)
+y_pred[0] = np.reshape(y_pred[0], y_pred[0].shape[:-1])
+# y_pred = {out.name.split(':')[0]: y_pred[i] for i, out in enumerate(model.outputs)}
+dtypes = np.dtype([("out_0", "float32"), ("out_1", "float32", (2,2))])
+
+result = np.array(y_pred, dtypes)
+
+a = np.array( [[1,2], [3,4], [5,6]] )
+aty = np.dtype([("asd", "float32", (2,1))])
+b = np.array(a, aty)
