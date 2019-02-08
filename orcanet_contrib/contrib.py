eeb6b08ac26553d654f2e04ca0c9fbde0871c112
ViaFerrata <michimoser@onlinehome.de>
2019-02-08 13:57:30 +0100

    Add configs for regression model.

--- orcanet_contrib/contrib.py
+++ orcanet_contrib/contrib.py
@@ -123,7 +123,7 @@ def orca_label_modifiers(class_type):
             # correct energy to visible energy
             visible_energy = y_values[elec_nc_bool_idx]['energy'] * y_values[elec_nc_bool_idx]['bjorkeny']
             # fix energy to visible energy
-            np.place(y_values['energy'], elec_nc_bool_idx, visible_energy)  # TODO fix numpy warning
+            np.place(y_values['energy'], elec_nc_bool_idx, visible_energy)
             # set bjorkeny label of nc events to 1
             np.place(y_values['bjorkeny'], elec_nc_bool_idx, 1)
 
